a2504d7452460b28b852308c81b6a4ef81c2ebb4
Lekseit <lekseir88@gmail.com>
2026-01-22 20:25:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('app.log', encoding='utf-8'),
+        logging.StreamHandler()
+    ]
+)
 def print_hi(name):
-
-    print(f'Hi, {name}')
+    logging.info(f'Вызов функции print_hi с аргументом: {name}')
+    if name:
+        print(f'Hi, {name}')
+        logging.info(f'Сообщение успешно создано: hi, {name}')
+    else:
+        logging.warning('Аргумент name не передан или пуст')
 
 if __name__ == '__main__':
-    print_hi(name=input("Введите свое имя: "))
+    logging.info('Запуск программы')
+    try:
 
+        print_hi(name=input("Введите свое имя: "))
+    except Exception as e:
+        logging.error(f'Произошла ошибка: {e}')
+    logging.info('Завершение программы')
 

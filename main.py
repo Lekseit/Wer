3e2f8a01539716dc5e6305ac67654d0044639e6c
Lekseit <lekseir88@gmail.com>
2026-01-22 19:57:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 
 def print_hi(name):
 
-    print(f'Hi, {name}'
+    print(f'Hi, {name}')
 
 if __name__ == '__main__':
-    print_hi('ветка main')
-    print("До свидания Алексей!!!!")
-    print("Пока")
+    print_hi(name=input("Введите свое имя: "))
+
 

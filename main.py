0062f4c38c3ab10f9db35d3337e751c4662c5e93
Lekseit <lekseir88@gmail.com>
2026-02-01 20:19:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 def print_hi(name):
 
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 if __name__ == '__main__':
     print_hi('ветка main')
-    print("До свиданияяяяяяяя")
+    print("До свиданияяяяяяяя!")
 
